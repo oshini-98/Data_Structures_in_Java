03317279b17266fed889a05c5a1d662f93011d6f
oshini-98 <76173707+oshini-98@users.noreply.github.com>
2023-10-05 03:53:04 +0530

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,3 +1,7 @@
+// In linked list data structure, data elements are connected through a series of nodes. 
+// And, each node contains the data items and address to the next node.
+
+
 package LinkedList;
 
 public class LinkedList {
